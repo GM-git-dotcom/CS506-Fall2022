f2542cb980092937f9fdd4e84b62ba0f2e0d7e6c
GM Harshvardhan <harrshvardhan@gmail.com>
2022-09-22 16:22:57 -0400

    Added sim and read, added minkowski dist, U54547249, GM Harshvardhan

--- labs/02-lab/cs506/read.py
+++ labs/02-lab/cs506/read.py
@@ -1,6 +1,19 @@
+import csv
 def read_csv(csv_file_path):
     """
         Given a path to a csv file, return a matrix (list of lists)
         in row major.
     """
-    raise NotImplementedError()
+    f = open(csv_file_path)
+    
+    csvreader = csv.reader(f)
+    res = []
+    for row in csvreader:
+        for i in range(len(row)):
+            if type(int(row[i])) == int:
+                row[i] = int(row[i])
+            else:
+        res.append(row)
+                
+
+    return res
--- labs/02-lab/cs506/sim.py
+++ labs/02-lab/cs506/sim.py
@@ -1,3 +1,5 @@
+import math
+
 def euclidean_dist(x, y):
     res = 0
     for i in range(len(x)):
@@ -5,12 +7,36 @@ def euclidean_dist(x, y):
     return res**(1/2)
 
 def manhattan_dist(x, y):
-    raise NotImplementedError()
+    #raise NotImplementedError()
+    s = 0
+    for i in range(len(x)):
+        s += abs(x[i] - y[i])
+    return s
 
 def jaccard_dist(x, y):
-    raise NotImplementedError()
+    #raise NotImplementedError()
+    intersect = len(list(set(x))).intersection(y)
+    union = (len(x) + len(y)) - intersect
+    return float(intersect)/union
 
 def cosine_sim(x, y):
-    raise NotImplementedError()
+    #raise NotImplementedError()
+    sumxx, sumxy, sumyy = 0, 0, 0
+    for i in range(len(x)):
+        temp1 = x[i]
+        temp2 = y[i]
+        sumxx +=temp1*temp1
+        sumyy += temp2*temp2
+        sumxy += temp1*temp2
+    return sumxy/math.sqrt(sumxx*sumyy)
+
+def minkowski_dist(x, y, p):
+    s = 0
+    for i in range(len(x)):
+        s += abs(x[i] = y[i])**p
+    return res**(1/p)
+
+
+    
 
 # Feel free to add more
